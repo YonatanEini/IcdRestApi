dcfa05cb8745fb2242dc565b0f95c028a1df1b26
YonatanEini <55375356+YonatanEini@users.noreply.github.com>
2022-01-11 16:54:14 +0200

    Delete Controllers directory

--- Controllers/RequestHandlers/CommunicationsType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace IcdFilesRestApi.Controllers.RequestsHandlers
-{
-    public enum EnumCommunicationType { Uplink, Downlink };
-}
